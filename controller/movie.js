f411d05c37460d8eef2b9ee1e112a768496f653a
albert-aristo <albertguo38@gmail.com>
2021-08-31 11:15:43 +0700

    final

--- controller/movie.js
+++ controller/movie.js
@@ -21,46 +21,60 @@ class movie{
     }
 
     static addMovie(req, res, next){
-        console.log(req.file);
-        // let { title, synopsis, trailerUrl, imgUrl, rating, genreId} = req.body
-        // let newMovie = { title, synopsis, trailerUrl, imgUrl, rating, genreId, authorId: req.userLogin.id}
-        // Movie.create(newMovie)
-        // .then((data) => {
-        //     res.status(201).json(data)
-        // })
-        // .catch((err) => {
-        //     if(err.name == 'SequelizeForeignKeyConstraintError'){
-        //         next({name: 'SequelizeForeignKeyConstraintError'})
-        //     }else if(err.errors[0].message == 'Validation min on rating failed'){
-        //         next({name: 'Validation min on rating failed'})
-        //     }else if(err.errors[0].message == 'Validation notEmpty on title failed'){
-        //         next({name: 'Validation notEmpty on title failed'})
-        //     }else{
-        //         next({name: ''})
-        //     }
-        // })
+        // console.log(req.file);
+        let { title, synopsis, trailerUrl, rating, genreId} = req.body
+        let newMovie = { title, synopsis, trailerUrl, imgUrl:req.body.imageUrl , rating, genreId, authorId: req.userLogin.id}
+        Movie.create(newMovie)
+        .then((data) => {
+            // console.log(data);
+            res.status(201).json(data)
+        })
+        .catch((err) => {
+            console.log(err);
+            if(err.name == 'SequelizeForeignKeyConstraintError'){
+                next({name: 'SequelizeForeignKeyConstraintError'})
+            }else if(err.errors[0].message == 'Validation min on rating failed'){
+                next({name: 'Validation min on rating failed'})
+            }else if(err.errors[0].message == 'Validation notEmpty on title failed'){
+                next({name: 'Validation notEmpty on title failed'})
+            }else{
+                next({name: ''})
+            }
+        })
     }
 
     static updateRow(req, res, next){
-        console.log(req.file);
-        console.log(req.body);
-        // let { title, synopsis, trailerUrl, imgUrl, rating, genreId, authorId} = req.body
-        // let newMovie = { title, synopsis, trailerUrl, imgUrl, rating, genreId, authorId}
-        // Movie.update(newMovie,{where: {id: req.params.id}, returning: true})
-        // .then((data) => {
-        //     res.status(200).json(data[1][0])
-        // })
-        // .catch((err) => {
-        //     if(err[0] == 0){
-        //         next({name: 'id not available'})
-        //     }else if(err.errors[0].message == 'Validation notEmpty on title failed'){
-        //         next({name: 'Validation notEmpty on title failed'})
-        //     }else if(err.errors[0].message == 'Validation min on rating failed'){
-        //         next({name: 'Validation min on rating failed'})
-        //     }else{
-        //         next({name: ''})
-        //     }
-        // })
+        let image = req.body.imageUrl
+        // console.log(image);
+        // console.log(req.file);
+        // console.log(req.body);
+        let { title, synopsis, trailerUrl, rating, genreId} = req.body
+        let newMovie = { title, synopsis, trailerUrl, rating, genreId, authorId: req.userLogin.id , imgUrl : image}
+        Movie.update(newMovie,{where: {id: req.params.id}, returning: true})
+        .then((data) => {
+            if(data){
+                res.status(200).json(data[1][0])
+            }else if(err[0] == 0){
+                next({name: 'id not available'})
+            }else if(err.errors[0].message == 'Validation notEmpty on title failed'){
+                next({name: 'Validation notEmpty on title failed'})
+            }else if(err.errors[0].message == 'Validation min on rating failed'){
+                next({name: 'Validation min on rating failed'})
+            }else{
+                next({name: ''})
+            }
+        })
+        .catch((err) => {
+            if(err[0] == 0){
+                next({name: 'id not available'})
+            }else if(err.errors[0].message == 'Validation notEmpty on title failed'){
+                next({name: 'Validation notEmpty on title failed'})
+            }else if(err.errors[0].message == 'Validation min on rating failed'){
+                next({name: 'Validation min on rating failed'})
+            }else{
+                next({name: ''})
+            }
+        })
     }
 
     static delete(req, res, next){
--- helpers/imagekit.js
+++ helpers/imagekit.js
@@ -10,33 +10,51 @@ async function imageKit(req, res, next) {
     form.append('file', req.file.buffer.toString('base64'))
     form.append('fileName', req.file.originalname)
 
-    const privateKey = new Buffer.from('private_J4COuqze2g3LPpYYWR0WgDi/ceU=' + ":").toString("base64");
+    const privateKey = new Buffer.from('private_UTPANzRP5bredNzyQvKwjdO7GVE=' + ":").toString("base64");
     
-    const uploader = await axios.post(
-      "https://upload.imagekit.io/api/v1/files/upload",
-      form,
-      {
-        headers: {
-          ...form.getHeaders(),
-          Authorization: `Basic ${privateKey}`,
-        },
+    // const uploader = await axios.post(
+    //   "https://upload.imagekit.io/api/v1/files/upload",
+    //   form,
+    //   {
+    //     headers: {
+    //       ...form.getHeaders(),
+    //       Authorization: `Basic ${privateKey}`,
+    //     },
+    //   }
+    // );
+    axios({
+      method: 'POST',
+      url: 'https://upload.imagekit.io/api/v1/files/upload',
+      data: form,
+      headers: {
+        ...form.getHeaders(),
+        Authorization: `Basic ${privateKey}`,
       }
-    );
+    })
+    .then((data) =>{
+      req.body.imageUrl = data.data.url
+      next()
+    })
+    .catch((err) => {
+      console.log(err);
+      next({msg: 'error image'})
+    })
 
-    let image;
-    console.log(uploader.data)
-    let formatFileImg = uploader.data.name.substring(uploader.data.name.lastIndexOf('.') + 1).toLowerCase()
-    if (formatFileImg === 'png') {
-      throw ({ name: "format file image cant be PNG" })
-    } else {
-      if (req.file.size >= 255000) {
-        throw ({ name: "file size excedeed the maximum size" })
-      } else {
-        image = uploader.data.url;
-      }
-    }
-    req.body.posterUrl = image
-    next()
+
+    // let image;
+    // console.log(uploader.data)
+    // let formatFileImg = uploader.data.name.substring(uploader.data.name.lastIndexOf('.') + 1).toLowerCase()
+    // if (formatFileImg === 'png') {
+    //   throw ({ name: "format file image cant be PNG" })
+    // } else {
+    //   if (req.file.size >= 255000) {
+    //     throw ({ name: "file size excedeed the maximum size" })
+    //   } else {
+    //     image = uploader.data.url;
+    //   }
+    // }
+    // req.body.posterUrl = image
+    // next()
   }
 }
 
